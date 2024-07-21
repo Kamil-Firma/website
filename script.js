c2a7480e54648c3932bcf214ce380daaf3c1162a
Kamil-Firma <147884520+Kamil-Firma@users.noreply.github.com>
2024-07-21 16:19:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-pressed_logo = 0;
+//clicking rabbitdud ------------------------------------------------------------
+played_theme = 0;
 
 function play_theme(){
-	if(pressed_logo==0){
+	if(played_theme==0){
 		var audio = new Audio( 'assets/theme.mp3' );
 		audio.play();
-		pressed_logo = 1;
+		played_theme = 1;
+		document.getElementById("piano").style.visibility = "visible";
+
 	}
 }
 
+//highlight carousel ------------------------------------------------------------
 carousel_images = [
 	'ruin2_s1.png',
 	'ruin2_s2.png',
@@ -34,3 +38,37 @@ function highlight_change_image(){
 function highlight_update_image(){
 	document.getElementById("highlight_carousel").innerHTML = "<img src='highlight/"+carousel_images[carousel_id]+"' width=650 onclick='highlight_change_image()' style='cursor: pointer'>";
 }
+
+//piano ------------------------------------------------------------
+document.getElementById("piano").style.visibility = "hidden";
+
+pianokeys = [
+	'piano/C.mp3',
+	'piano/Cs.mp3',
+	'piano/D.mp3',
+	'piano/Ds.mp3',
+	'piano/E.mp3',
+	'piano/F.mp3',
+	'piano/Fs.mp3',
+	'piano/G.mp3',
+	'piano/Gs.mp3',
+	'piano/A.mp3',
+	'piano/As.mp3',
+	'piano/B.mp3'
+]
+
+wrongnote = 'piano/wrong.mp3';
+wrongnote_random = 0;
+
+function pianokey(key){
+	wrongnote_random = Math.floor(Math.random() * 20);
+	if(wrongnote_random==1){
+		var toplay = wrongnote;
+	}else{
+		var toplay = pianokeys[key]
+	}
+	
+	var audio = new Audio( toplay );
+	audio.play();
+}
+
